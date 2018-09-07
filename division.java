ce895fb0c34418d77fee181e67bcbe227a2d3430
aarusaru <43059566+aarusaru@users.noreply.github.com>
2018-09-07 12:22:49 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int c;
+    int a=0;
+    int b=1;
+     c=a/b;
+    System.out.println(c);
+  }
+}class Main {
+  public static void main(String[] args) {
+    int c;
+    int a=0;
+    int b=1;
+     c=a/b;
+    System.out.println(c);
+  }
+}
