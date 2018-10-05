64eeb95b0db9ba95420cc4befeea60346a25dd7f
aarusaru <43059566+aarusaru@users.noreply.github.com>
2018-10-05 12:09:02 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,15 @@
+class Main
+{ 
+    public static boolean isEven(int n) 
+    { 
+    return (n % 2 == 0); 
+    } 
+    public static void main(String[] args) 
+    { 
+    int n =101; 
+        if(isEven(n) == true) 
+            System.out.print("Even"); 
+        else
+            System.out.print("Odd"); 
+    } 
+} 
